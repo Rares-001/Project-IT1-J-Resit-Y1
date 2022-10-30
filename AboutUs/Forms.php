fa00a0b33cba8b2179a502b6325a6c9bcbaad5ab
Oliverherf <91316579+Oliverherf@users.noreply.github.com>
2022-10-30 02:31:27 +0100

    Added updated the my php a little

--- AboutUs/Forms.php
+++ AboutUs/Forms.php
@@ -10,37 +10,48 @@
         </style>
     </head>
     <body>
-        <form action="forms.PHP" method="post" name="form">
-        <input name="name" type="text" placeholder="Your name"/>
-        <br>
-        <input name="email" type="text" placeholder="Email"/>
-        <br>
-        <textarea cols="32" name="message" rows="5"> Your Feedback
-        </textarea>
-        <br>
-        <input type="submit" value="Submit" />
+        <form action="Forms.php" method="post" name="form">
+            <input name="name" type="text" placeholder="Your name"/>
+            <br>
+            <input name="email" type="text" placeholder="Email"/>
+            <br>
+            <textarea cols="32" name="message" rows="5">Feedback text
+            </textarea>
+            <br>
+            <input type="submit" value="Submit" />
         </form>
+        <br>
+        <p><a href="AboutUs.html">Go Back to About us Page</a></p>
 
         <?php 
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
+        
+        if(isset($_POST['Submit'])){
+            if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['message'])){
 
-        if(strlen($_POST['name'])>= 3){
-            if(str_contains($_POST['email'], "@" )){
-                if(strlen($_POST['message'])>= 20){
-                    echo "FeedBack has been sent.";
-                    echo "Thank you for filling in the form";
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $message = $_POST['message'];
+
+                if(strlen($_POST['name'])>= 3){
+                    if(str_contains($_POST['email'], "@" )){
+                        if(strlen($_POST['message'])>= 20){
+                            echo "FeedBack has been sent.";
+                            echo "Thank you for filling in the form";
+                        }else{
+                            echo "The feedback must Contain at least 20 characters.";
+                        }
+    
+                    }else{
+                        echo "Fill in the E-mail address.";
+                    }
                 }else{
-                    echo "The feedback must Contain at least 20 characters.";
+                    echo "Fill in First name.";
                 }
-
+                
             }else{
-                echo "Fill in the E-mail address.";
+                echo"Fill in all the fields";
             }
-        }else{
-            echo "Fill in First name.";
-        }
+        }   
         ?>
 
     </body>
